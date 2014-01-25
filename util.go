6ce06eafbc26f3c6178a250330e839379c086373
BeMasher <bemasher@gmail.com>
2014-01-25 06:56:35 -0700

    Moved utility functions for testing to util.go. Added FFT-flavored versions.

--- util.go
+++ util.go
@@ -1,5 +1,10 @@
 package fnt
 
+import (
+	"math/rand"
+	"time"
+)
+
 // Constants for selecting algorithm type.
 type DivisionKind int
 
@@ -8,8 +13,55 @@ const (
 	DIF                     // Division-in-Frequency
 )
 
+var div = []DivisionKind{DIT, DIF}
+
+type Direction int
+
+const (
+	Forward Direction = iota
+	Backward
+)
+
+const Tolerance = 1e-12
+
 func log2(n uint) (log uint) {
 	for ; n > 1; log, n = log+1, n>>1 {
 	}
 	return
+}
+
+func randBlockF64(n int) []float64 {
+	samples := make([]float64, n)
+	for i := range samples {
+		samples[i] = rand.Float64()
+	}
+	return samples
+}
+
+func randBlockC128(n int) []complex128 {
+	samples := make([]complex128, n)
+	for i := range samples {
+		samples[i] = complex(rand.Float64(), rand.Float64())
+	}
+	return samples
+}
+
+func stepResponseF64(n int) []float64 {
+	samples := make([]float64, n)
+	for i := 0; i < n>>1; i++ {
+		samples[i] = 1.0
+	}
+	return samples
+}
+
+func stepResponseC128(n int) []complex128 {
+	samples := make([]complex128, n)
+	for i := 0; i < n>>1; i++ {
+		samples[i] = 1.0
+	}
+	return samples
+}
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
 }
